25fc8459299882a7663fd4126cb4dea6fb75175d
Robert Langhammer <langhammer.robert@t-online.de>
2024-11-30 12:54:16 +0100

    Features Added | Sperated User and Lib

--- __init__.py
+++ __init__.py
@@ -1,52 +1,60 @@
+import os
+_CWD = os.getcwd()
+os.chdir(os.path.split(__file__)[0])
+from toolLib import (_CONF,
+                     _Printer,
+                     _Exit,
+                     _title,
+                     _lock,
+                     _run)
 
 # python-IDLE-Tools
 
 ## CONFIG ##
-_CLEAR_ON_START = True
+_CONF._CLEAR_ON_START = True
+_CONF._CLEAR_ON_EXIT = True
+_CONF._CAN_EXIT_CTRL_C = True
 
 ## IMPORTS ##
 from math import *
 ############
 
+#### USER DEFINED FUNCTIONS ####
+def egcd(a, b, x=1, xx=0, y=0, yy=1, printStep=False):
+   c = lambda a, b, q:(a-q*b)
+
+   if printStep: print(f"{a=}, {b=}, {x=}, {xx=}, {y=}, {yy=}")
+   if b > 0:
+       q = a//b
+       egcd(b, c(a, b, q), xx, c(x, xx, q), yy, c(y, yy, q), printStep)
+   else:
+        return x
+
+def isbn10(isbn:str)->bool:
+    if len(isbn) != 10:
+        print("isbn is not length 10.")
+        return False
+    _isbnArr = [int(j)*(10-i) for i, j in enumerate(isbn)]
+    return sum(_isbnArr)%11 == 0
+
+def isbn13(isbn:str)->bool:
+    if len(isbn) != 13:
+        print("isbn is not length 13.")
+        return False
+    _sum = 0
+    for i, j in enumerate(isbn):
+        _sum += int(j) * (1 if i%2==0 else 3)
+    return _sum%10 == 0
+
+def isbn10x(isbn:str)->int:
+    if len(isbn) != 9:
+        print("isbn is not length 9.")
+        return False
+    _isbnArr = [int(j)*(10-i) for i, j in enumerate(isbn)]
+    return sum(_isbnArr)%11 == 0
 
 
-
-
-
-
-
-
-
-import sys, os
-from colorama import init as _init, Fore as _c
-def _lock():
-    _lockVar = os.environ.get("RUN_LOCK", "false")
-    if _lockVar == "false":
-        os.environ["RUN_LOCK"] = "true"
-        return True
-    return False
-
-def _title(_s=False):
-    if not _s or _CLEAR_ON_START: os.system("clear")
-    c = sys.version_info
-    print(_c.GREEN+f"Python-{c.major}.{c.minor}.{c.micro}"+_c.RESET)
-    print(_c.GREEN+f"Tool-Version: {VERSION} | \u00A9 Robert Langhammmer"+_c.RESET)
-class _Printer:
-    def __init__(self, commands=None, return_="", desc="", usage=""):
-        self.commands = commands
-        self.return_ = return_
-
-    def __repr__(self):
-        for i in self.commands:
-            i()
-        return self.return_
-class _Exit(_Printer):
-    def __init__(self, commands=None, return_="", desc="", usage=""):
-        super().__init__(commands, return_, desc, usage)
-
-    def __call__(self, *args, **kwargs):
-        os._exit(0)
-## commands ##
+#### USER  DEFINED COMMANDS ####
 clear = cls = _Printer(
     commands=[
         _title
@@ -57,11 +65,9 @@ clear = cls = _Printer(
 )
 exit = _Exit(
     commands=[
+        lambda: os.system("clear"),
         lambda: os._exit(0)
-    ]
+    ] if _CONF._CLEAR_ON_START else [lambda: os._exit(0)]
 )
 if __name__ == '__main__':
-    if _lock():
-        _init()
-        VERSION = 1.2
-        _title(True)
+    _run()
---
+++ toolLib.py
@@ -0,0 +1,48 @@
+import os, sys
+from colorama import init as _init, Fore as _c
+
+_init()
+VERSION = 1.2
+
+def _run():
+    try:
+        if _lock():
+            _title(True)
+    except KeyboardInterrupt:
+        os._exit(0)
+
+
+class _CONF:
+    _CLEAR_ON_START = None
+    _CLEAR_ON_EXIT = None
+    _CAN_EXIT_CTRL_C = None
+
+def _lock():
+    _lockVar = os.environ.get("RUN_LOCK", "false")
+    if _lockVar == "false":
+        os.environ["RUN_LOCK"] = "true"
+        return True
+    return False
+
+def _title(_s=False):
+    if not _s or _CONF._CLEAR_ON_START: os.system("clear")
+    c = sys.version_info
+    print(_c.GREEN+f"Python-{c.major}.{c.minor}.{c.micro}"+_c.RESET)
+    print(_c.GREEN+f"Tool-Version: {VERSION} | \u00A9 Robert Langhammmer"+_c.RESET)
+
+
+class _Printer:
+    def __init__(self, commands=None, return_="", desc="", usage=""):
+        self.commands = commands
+        self.return_ = return_
+
+    def __repr__(self):
+        for i in self.commands:
+            i()
+        return self.return_
+class _Exit(_Printer):
+    def __init__(self, commands=None, return_="", desc="", usage=""):
+        super().__init__(commands, return_, desc, usage)
+
+    def __call__(self, *args, **kwargs):
+        os._exit(0)
